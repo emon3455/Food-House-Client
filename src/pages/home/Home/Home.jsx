9c388eaaa7fe62371323b1ae4e121c6b3671d11a
Emon <emon.mhk69@gmail.com>
2023-05-23 02:06:58 +0600

    catagory done with swiper js sldier

--- src/pages/home/Home/Home.jsx
+++ src/pages/home/Home/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "../banner/Banner";
+import Catagory from "../catagory/Catagory";
 
 const Home = () => {
     return (
         <div className="text-center mb-5">
             <Banner></Banner>
+            <div className="space-y-5">
+                <Catagory></Catagory>
+            </div>
         </div>
     );
 };
