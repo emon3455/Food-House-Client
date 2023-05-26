e3d8d67ff340d9c817cbe72f9b4a9fab5980c281
Emon <emon.mhk69@gmail.com>
2023-05-27 04:03:17 +0600

    log in log out register done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,24 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
 import { Link } from "react-router-dom";
 import login from "../../assets/others/authentication1.png";
 import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { AuthContext } from "../../provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const Login = () => {
 
-    const ref= useRef(null);
+    const {signInUser} = useContext(AuthContext);
+
+    const ref = useRef(null);
     const [disable, setDisble] = useState(true);
 
-    useEffect(()=>{
-        loadCaptchaEnginge(6); 
-    },[])
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, [])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,75 +26,101 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
 
-        console.log(email, password);
+        signInUser(email,password)
+        .then(res => {
+            const logedUser = res.user;
+            if (logedUser) {
+                Swal.fire(
+                    'Successfully Loged In!',
+                    'Success!',
+                    'success'
+                )    
+            }
+            else {
+                return;
+            }
+        })
+        .catch(er => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong!',
+            })
+        })
     }
 
-    const handleCaptaValidation=()=>{
+    const handleCaptaValidation = () => {
         const userCapta = ref.current.value;
-        if (validateCaptcha(userCapta)==true) {
+        if (validateCaptcha(userCapta) == true) {
             setDisble(false);
         }
-   
+
         else {
             setDisble(true);
         }
     }
 
     return (
-        <div className='mt-10' >
-            <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center gap-5 p-2">
-                <div className="card w-full max-w-sm drop-shadow-2xl bg-base-100 order-2 md:order-1">
-                    <form onSubmit={handleSubmit} className="card-body">
-                        <h2 className="text-3xl font-bold text-center">Login</h2>
-                        <p className="text-center text-red-600">  </p>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" placeholder="email" name="email" id="email" className="p-2 border-2 rounded-lg w-full" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" id="password" name="password" placeholder="password" className="p-2 border-2 rounded-lg w-full" />
+        <>
+
+            <Helmet>
+                <title>Food house | LogIn</title>
+            </Helmet>
+            <div className='mt-10' >
+                <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center gap-5 p-2">
+                    <div className="card w-full max-w-sm drop-shadow-2xl bg-base-100 order-2 md:order-1">
+                        <form onSubmit={handleSubmit} className="card-body">
+                            <h2 className="text-3xl font-bold text-center">Login</h2>
+                            <p className="text-center text-red-600">  </p>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" name="email" id="email" className="p-2 border-2 rounded-lg w-full" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" id="password" name="password" placeholder="password" className="p-2 border-2 rounded-lg w-full" />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <LoadCanvasTemplate />
+                                </label>
+                            </div>
+
+                            <div className="flex justify-between gap-4">
+                                <input type="text" id="capta" ref={ref} name="capta" placeholder="type here" className="p-2 border-2 rounded-lg w-full" />
+                                <button onClick={handleCaptaValidation} className="btn p-2">Check</button>
+                            </div>
+
+                            <input type="submit" value="Login" className="btn btn-warning btn-sm mt-2" disabled={disable} />
+                        </form>
+                        <div className="px-4 py-2">
+                            <p className="text-center text-gray-600">
+                                Don't have an Account? <Link className="text-sky-600" to="/register">Create an account</Link>
+                            </p>
+                            <p className="text-center font-bold">
+                                OR Login With
+                            </p>
+                            <div className="flex justify-evenly">
+                                <span className="p-4 btn-ghost rounded-full text-sky-600 text-lg"><FaFacebookF></FaFacebookF></span>
+                                <span className="p-4 btn-ghost rounded-full text-orange-400 text-lg"><FaGoogle></FaGoogle></span>
+                                <span className="p-4 btn-ghost rounded-full text-lg"><FaGithub></FaGithub></span>
+                            </div>
                         </div>
 
-                        <div className="form-control">
-                            <label className="label">
-                                <LoadCanvasTemplate />
-                            </label>
-                        </div>
-
-                        <div className="flex justify-between gap-4">
-                            <input type="text" id="capta" ref={ref} name="capta" placeholder="type here" className="p-2 border-2 rounded-lg w-full" />
-                            <button onClick={handleCaptaValidation} className="btn p-2">Check</button>
-                        </div>
-
-                        <input type="submit" value="Login" className="btn btn-warning btn-sm mt-2" disabled={disable}/>
-                    </form>
-                    <div className="px-4 py-2">
-                        <p className="text-center text-gray-600">
-                            Don't have an Account? <Link className="text-sky-600" to="/register">Create an account</Link>
-                        </p>
-                        <p className="text-center font-bold">
-                            OR Login With
-                        </p>
-                        <div className="flex justify-evenly">
-                            <span className="p-4 btn-ghost rounded-full text-sky-600 text-lg"><FaFacebookF></FaFacebookF></span>
-                            <span className="p-4 btn-ghost rounded-full text-orange-400 text-lg"><FaGoogle></FaGoogle></span>
-                            <span className="p-4 btn-ghost rounded-full text-lg"><FaGithub></FaGithub></span>
-                        </div>
                     </div>
+                    <div className="max-w-lg order-1 md:order-2">
+                        <img className="w-full" src={login} alt="login image" />
+                    </div>
+                </div>
 
-                </div>
-                <div className="max-w-lg order-1 md:order-2">
-                    <img className="w-full" src={login} alt="login image" />
-                </div>
+
             </div>
-
-
-        </div>
+        </>
     );
 };
 
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,70 +1,129 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
 import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import img from "../../assets/others/authentication1.png"
+import { useForm } from "react-hook-form";
+import { Helmet } from "react-helmet-async";
+import { useContext } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+import Swal from "sweetalert2";
+import { updateProfile } from "firebase/auth";
 
 const SignUp = () => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const name= form.name.value;
-        const email = form.email.value;
-        const password = form.password.value;
+    const { createUser } = useContext(AuthContext);
 
-        console.log(name,email, password);
-    }
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+        reset();
+        createUser(data.email, data.password)
+            .then(res => {
+                const createdUser = res.user;
+                if (createdUser) {
+                    Swal.fire(
+                        'User created Successfully!',
+                        'Success!',
+                        'success'
+                    )
+                    updateProfile(createdUser, {
+                        displayName: data.name
+                    }).then(() => {
+                        Swal.fire(
+                            'User Profile Updated!',
+                            'Success!',
+                            'success'
+                        )
+                    }).catch((error) => {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Sorry user profile Cannot Updated!',
+                        })
+                    });
+                    
+                }
+                else {
+                    return;
+                }
+            })
+            .catch(er => {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                })
+            })
+    };
 
     return (
-        <div className='mt-10' >
-            <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center gap-5 p-2">
-                <div className="card w-full max-w-sm drop-shadow-2xl bg-base-100 order-2 md:order-1">
-                    <form onSubmit={handleSubmit} className="card-body">
-                        <h2 className="text-3xl font-bold text-center">Sign Up</h2>
-                        <p className="text-center text-red-600">  </p>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Name</span>
-                            </label>
-                            <input type="text" name="name" placeholder="name" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" placeholder="email" name="email" id="email" className="p-2 border-2 rounded-lg w-full" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" id="password" name="password" placeholder="password" className="p-2 border-2 rounded-lg w-full" />
+        <>
+
+            <Helmet>
+                <title>Food house | SignUp</title>
+            </Helmet>
+            <div className='mt-10' >
+                <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center gap-5 p-2">
+                    <div className="card w-full max-w-sm drop-shadow-2xl bg-base-100 order-2 md:order-1">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            <h2 className="text-3xl font-bold text-center">Sign Up</h2>
+                            <p className="text-center text-red-600">  </p>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name="name" {...register("name", { required: true })} placeholder="name" className="input input-bordered" />
+                                {errors.name && <span className="text-red-600 ">name is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" name="email" {...register("email", { required: true })} className="p-2 border-2 rounded-lg w-full" />
+                                {errors.email && <span className="text-red-600 ">email is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password"  {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                })} placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
+                            </div>
+
+                            <input type="submit" value="Sign Up" className="btn btn-warning btn-sm mt-2" />
+                        </form>
+                        <div className="px-4 py-2">
+                            <p className="text-center text-gray-600">
+                                Already have an Account? <Link className="text-sky-600" to="/login">Login</Link>
+                            </p>
+                            <p className="text-center font-bold">
+                                OR SignUp With
+                            </p>
+                            <div className="flex justify-evenly">
+                                <span className="p-4 btn-ghost rounded-full text-sky-600 text-lg"><FaFacebookF></FaFacebookF></span>
+                                <span className="p-4 btn-ghost rounded-full text-orange-400 text-lg"><FaGoogle></FaGoogle></span>
+                                <span className="p-4 btn-ghost rounded-full text-lg"><FaGithub></FaGithub></span>
+                            </div>
                         </div>
 
-                        <input type="submit" value="Sign Up" className="btn btn-warning btn-sm mt-2"/>
-                    </form>
-                    <div className="px-4 py-2">
-                        <p className="text-center text-gray-600">
-                            Already have an Account? <Link className="text-sky-600" to="/login">Login</Link>
-                        </p>
-                        <p className="text-center font-bold">
-                            OR SignUp With
-                        </p>
-                        <div className="flex justify-evenly">
-                            <span className="p-4 btn-ghost rounded-full text-sky-600 text-lg"><FaFacebookF></FaFacebookF></span>
-                            <span className="p-4 btn-ghost rounded-full text-orange-400 text-lg"><FaGoogle></FaGoogle></span>
-                            <span className="p-4 btn-ghost rounded-full text-lg"><FaGithub></FaGithub></span>
-                        </div>
                     </div>
+                    <div className="max-w-lg order-1 md:order-2">
+                        <img className="w-full" src={img} alt="login image" />
+                    </div>
+                </div>
 
-                </div>
-                <div className="max-w-lg order-1 md:order-2">
-                    <img className="w-full" src={img} alt="login image" />
-                </div>
+
             </div>
-
-
-        </div>
+        </>
     );
 };
 
